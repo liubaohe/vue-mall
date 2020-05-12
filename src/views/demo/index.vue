1ab25acec95760f38f02b00c4936c689f6831234
liu.baohe <liubaohe_lwdb@163.com>
2020-05-12 17:51:21 +0800

    ssss

---
+++ src/views/demo/index.vue
@@ -0,0 +1,69 @@
+<template>
+    <el-container>
+        <el-header>
+            <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                <el-menu-item index="1">处理中心</el-menu-item>
+                <el-submenu index="2">
+                    <template slot="title">我的工作台</template>
+                    <el-menu-item index="2-1">选项1</el-menu-item>
+                    <el-menu-item index="2-2">选项2</el-menu-item>
+                    <el-menu-item index="2-3">选项3</el-menu-item>
+                    <el-submenu index="2-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="2-4-1">选项1</el-menu-item>
+                    <el-menu-item index="2-4-2">选项2</el-menu-item>
+                    <el-menu-item index="2-4-3">选项3</el-menu-item>
+                    </el-submenu>
+                </el-submenu>
+                <el-menu-item index="3" disabled>消息中心</el-menu-item>
+                <el-menu-item index="4"><a href="" target="_blank">订单管理</a></el-menu-item>
+                </el-menu>
+        </el-header>
+        <el-main>
+          
+        </el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+}
+</script>
+
+<style scoped>
+.el-header{
+    line-height: 60px;
+}
+.el-footer{
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+}
+
+.el-footer{
+    position:fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+}
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+}
+body > .el-container {
+    margin-bottom: 40px;
+}
+
+</style>
